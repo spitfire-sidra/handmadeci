bad26f15d914aa87c8fbb23c1759bbfe22c0803f
Amo Chen <spitfire.sidra@gmail.com>
2015-09-08 22:31:05 +0800

    added src/hello_world.py

---
+++ src/hello_world.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+def return_hello_world():
+    return 'Hello World'
--- test-local.py
+++ test-local.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
 import subprocess
 
+
 def get_changed_files():
+
     stdout_str = subprocess.check_output([
         'git',
         '--no-pager',
@@ -13,6 +16,14 @@ def get_changed_files():
         '--ignore-space-at-eol',
         'HEAD~'
     ])
+
+    file_path_regex = re.compile(r'^.\s+(?P<file_path>.+)')
+    stdout_lines = stdout_str.split('\n')
+
+    for line in stdout_lines:
+        match_obj = file_path_regex.search(line)
+        if match_obj:
+            print match_obj.group('file_path')
 
 
 if __name__ == '__main__':
